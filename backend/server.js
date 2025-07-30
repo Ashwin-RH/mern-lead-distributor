67b0e58b2112ba655dfe1faeca078ac7fbd91fdf
Ashwin-RH <ashwinharagi@gmail.com>
2025-07-30 22:18:46 +0530

    deleted some local files

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // ✅ CORS setup — allow frontend domain
 app.use(cors({
-  origin: ["http://localhost:5173", "https://azyn.vercel.app/"],
+  origin: ["http://localhost:5173", "https://azyn.vercel.app"],
   credentials: true,
 }));
 
